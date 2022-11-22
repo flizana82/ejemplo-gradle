55169b73f05a6191be3daf87846a23166e5f784c
Fabian Lizana <flizana@outlook.cl>
2022-11-21 23:20:11 -0300

    Prueba gradle 13

--- gradle.groovy
+++ gradle.groovy
@@ -4,10 +4,8 @@ def buildGradle(){
 }
 
 def sonarGradle(){
-    sh './gradlew sonar:sonar \
-  -Dsonar.projectKey=ejemplo-gradle2 \
-  -Dsonar.host.url=http://sonarqube \
-  -Dsonar.login=sqp_c802b263acfa81c1b72bf81411a6b70a58fba27e'
+    sh './gradlew sonarqube \
+        -Dsonar.projectKey=ejemplo-gradle'
 }
 
 def runAndTest(){
